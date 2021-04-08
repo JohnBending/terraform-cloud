3c08adae95c7942282fe0722311f355a0bd23a2e
JohnBending <47155839+JohnBending@users.noreply.github.com>
2021-04-09 00:48:28 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,63 @@
+data "aws_ami" "latest_amazon_linux" {
+  owners      = ["137112412989"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.latest_amazon_linux.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web.id]
+  user_data              = <<EOF
+#!/bin/bash
+yum -y update
+yum -y install httpd
+myip=`curl 2ip.ru`
+echo "<h2>PROD WebServer with IP: $myip</h2><br>Build by Terraform!" workspace-test  >  /var/www/html/index.html
+sudo service httpd start
+chkconfig httpd on
+EOF
+
+  tags = {
+    Name  = "PROD WebServer - ${terraform.workspace}"
+    Owner = "Ivan Andreev"
+  }
+}
+
+resource "aws_security_group" "web" {
+  name_prefix = "WebServer SG Prod"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name  = "Web Server SecurityGroup"
+    Owner = "Ivan Andreev"
+  }
+}
+
+resource "aws_eip" "web" {
+  instance = aws_instance.web.id
+  tags = {
+    Name  = "PROD WebServer IP"
+    Owner = "Denis Astahov"
+  }
+}
+
+####################################
+output "web_public_ip" {
+  value = aws_eip.web.public_ip
+}
