aa75a7cae8800a9fa11bab40915a1830d21d24c5
JohnBending <47155839+JohnBending@users.noreply.github.com>
2021-05-14 17:18:18 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,21 @@ resource "aws_instance" "web" {
   ami                    = data.aws_ami.latest_amazon_linux.id
   instance_type          = var.server_size
   vpc_security_group_ids = [aws_security_group.web.id]
-  user_data              = file("jenkins.sh")
+  user_data              = <<EOF
+#!/bin/bash
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
+    /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install -y jenkins
+sudo apt update
+sudo apt search openjdk
+sudo apt install -y openjdk-11-jdk
+sudo apt install -y openjdk-11-jdk
+java -version
+sudo systemctl daemon-reload
+sudo systemctl start jenkins
+EOF
 
   tags = {
     Name  = "${var.server_name}-WebServer"
